321a60bee32b478aefd2929d2149a4a42008a888
alexfelderean <alexfelderean@outlook.com>
2026-02-09 14:41:57 -0500

    base code

---
+++ songlink_app/android/app/src/main/kotlin/com/example/songlink_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.songlink_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ songlink_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import nsd_macos
+import sqlite3_flutter_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NsdMacosPlugin.register(with: registry.registrar(forPlugin: "NsdMacosPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+}
